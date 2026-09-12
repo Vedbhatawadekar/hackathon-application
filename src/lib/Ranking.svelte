c8aadeca97faab353a974e6f2691f3cd666cdbd3
Jakub Kranz <jakub.kranz1@gmail.com>
2026-09-12 16:54:47 +0200

    created ranking list

--- src/lib/Ranking.svelte
+++ src/lib/Ranking.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
   import RankingItem from "$lib/RankingItem.svelte";
+  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
+
+  const data = [
+    { stockName: "MSFT", score: 100 },
+    { stockName: "GOGL", score: 50 },
+    { stockName: "AAPL", score: 20 },
+  ]
 </script>
 
-<ol>
-  <RankingItem stockName="MSFT"/>
-  <RankingItem stockName="GOGL"/>
-  <RankingItem stockName="AAPL"/>
-</ol>
+<ScrollArea class="rounded-md border w-200 h-dvh m-auto my-10">
+  {#each data as stock, index}
+    <RankingItem stockName={stock.stockName} ranking={index + 1} score={stock.score}/>
+  {/each}
+</ScrollArea>
 
-
-<style>
-  ol {
-    size: 0 auto;
-    margin: 100px;
-    background-color: red;
-  }
-</style>
--- src/lib/RankingItem.svelte
+++ src/lib/RankingItem.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
-  let { stockName } = $props();
+  import * as Item from "$lib/components/ui/item/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Separator } from "$lib/components/ui/separator/index.js";
+
+  let { stockName, ranking, score } = $props();
 </script>
 
-
-<li>{stockName}</li>
+<div class="flex w-full max-w-md flex-col gap-6">
+  <Item.Root variant="outline" class="w-full">
+    <div class="text-center">
+      <p class="text-3xl">#{ranking}</p>
+      
+      <Separator />
+      Overall score:
+      <br/>
+      {score}
+    </div>
+    <Item.Content>
+      <Item.Title>{stockName}</Item.Title>
+      <Item.Description
+	>50%</Item.Description
+	      >
+    </Item.Content>
+    <Item.Actions>
+      <Button variant="outline" size="sm">Details</Button>
+    </Item.Actions>
+  </Item.Root>
+</div>
